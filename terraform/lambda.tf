b84e029fe036dce1a31ae3de28a56a5ff0013f5c
barak <schosterbarak@gmail.com>
2020-03-29 01:33:46 +0300

    add ec2 and lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,7 +18,8 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "analysis_lambda" {
+  # lambda have plain text secrets in environment variables
   filename      = "resources/lambda_function_payload.zip"
   function_name = "${local.resource_prefix.value}-analysis"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
