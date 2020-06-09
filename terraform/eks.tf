dc5abe246dd111273418aa74be418c62846e9e9b
Elay Gliksberg <elaygl@gmail.com>
2020-06-09 12:14:18 +0300

    Remove unnecessary comments

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -66,8 +66,6 @@ resource "aws_eks_cluster" "primary_eks" {
     subnet_ids = ["${aws_subnet.primary_eks_subnet1.id}", "${aws_subnet.primary_eks_subnet2.id}"]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     "aws_iam_role_policy_attachment.policy_attachment-AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.policy_attachment-AmazonEKSServicePolicy",
