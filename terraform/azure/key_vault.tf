f8ff847bb69370bbe03b3d2b70db586ff6c867fc
Adin Ermie <Adin.Ermie@outlook.com>
2020-06-19 17:16:08 -0400

    Corrected KeyVault Secret Resource
    Fixed KeyVault Secret resource code block (it was missing the quotes).

--- terraform/azure/key_vault.tf
+++ terraform/azure/key_vault.tf
@@ -36,7 +36,7 @@ resource "azurerm_key_vault_key" "generated" {
   ]
 }
 
-resource azurerm_key_vault_secret "secret" {
+resource "azurerm_key_vault_secret" "secret" {
   key_vault_id = azurerm_key_vault.example.id
   name         = "terragoat-secret-${var.environment}"
   value        = random_string.password.result
