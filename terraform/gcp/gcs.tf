51e72ba1bde361df8d86b0120b6cc11c27adc484
Nimrod Kor <nimrodkor@gmail.com>
2020-07-08 12:51:11 +0300

    Add force destroy to GCS bucket

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "bad_bucket" {
-  name = "terragot-${var.environment}"
+  name          = "terragot-${var.environment}"
+  force_destroy = true
 }
 
 resource "google_storage_bucket_iam_binding" "allow_public_read" {
