968b06ae7e49b721400458c8bf0735be677128ac
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-07-30 10:30:47 -0500

    Add second bucket for issues

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -8,3 +8,8 @@ resource "google_storage_bucket_iam_binding" "allow_public_read" {
   members = ["allUsers"]
   role    = "roles/storage.objectViewer"
 }
+
+resource "google_storage_bucket" "internal_storage" {
+  name          = "terragot-${var.environment}"
+  force_destroy = true
+}
