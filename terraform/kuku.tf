b1c734848c630f61034b4037d14627bb107feb1a
Or Evron <orevron@gmail.com>
2020-11-17 17:56:06 +0200

    etststst

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -1,18 +1,4 @@
-resource "aws_s3_bucket" "template_bucket" {
-  bucket        = "local.bucket_name"
-  force_destroy = true
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  versioning {
-    enabled = true
-  }
+variable "identifier" {
+  description = "The name of the RDS instance, if omitted, Terraform will assign a random, unique identifier"
+  type        = string
 }
-
-
-
-
