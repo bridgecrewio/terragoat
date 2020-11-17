900d88acbbc1e1775b1f95fe2020d7c6df2f9fe6
Or Evron <orevron@gmail.com>
2020-11-17 16:36:23 +0200

    fix

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -1,8 +1,13 @@
 resource "aws_s3_bucket" "template_bucket" {
   bucket        = "local.bucket_name"
-  acl           = "public-read"
   force_destroy = true
-
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   versioning {
     enabled = true
   }
