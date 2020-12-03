c5c7b14c19b2a1f152a6066d1f35f998683b9abc
mikeurbanski1 <mikeurbanski1@users.noreply.github.com>
2020-12-02 18:54:58 -0600

    Update module_usage.tf

--- terraform/aws/module_usage.tf
+++ terraform/aws/module_usage.tf
@@ -1,5 +1,13 @@
-module "sg" {
+module "sg1" {
   source = "./terraform/aws/modules/security_group"
+
+  port = 22
+  cidrs = ["0.0.0.0/0"]
+
+}
+
+module "sg2" {
+  source = "./modules/security_group"
 
   port = 22
   cidrs = ["0.0.0.0/0"]
