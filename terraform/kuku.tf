51432a78e77713012857a7abb12b1aa6e6267ca5
Or Evron <orevron@gmail.com>
2020-12-24 14:32:38 +0200

    reset

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -2,15 +2,6 @@ resource "aws_s3_bucket" "template_bucket" {
   bucket        = "local.bucket_name"
   force_destroy = true
 }
-
-
-
-
-
-
-
-
-
 resource "aws_s3_bucket" "template_bucket" {
   bucket        = "local.bucket_name"
   force_destroy = true
