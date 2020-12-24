a1ccb05a28a8d7fb4616edd2bbb95824eebfd892
Or Evron <orevron@gmail.com>
2020-12-24 12:30:49 +0200

    add violations

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -12,3 +12,33 @@ resource "aws_s3_bucket" "template_bucket" {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+resource "aws_s3_bucket" "template_bucket" {
+  bucket        = "local.bucket_name"
+  force_destroy = true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+resource "aws_s3_bucket" "template_bucket" {
+  bucket        = "local.bucket_name"
+  force_destroy = true
+}
