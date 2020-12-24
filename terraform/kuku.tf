e859dfc55d60f500693eeb9aea223d597caa23e5
Or Evron <orevron@gmail.com>
2020-12-24 12:33:12 +0200

    add violations

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -4,19 +4,6 @@ resource "aws_s3_bucket" "template_bucket" {
   versioning {
     enabled = true
   }
-  server_side_encryption_configuration {
-    
-    
-    
-    
-    
-    
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
 
 
