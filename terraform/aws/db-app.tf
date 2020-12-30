2523d93d3f21857628c03beab312ac0553f6f6d2
jonnyCodev <42693227+jonnyCodev@users.noreply.github.com>
2020-12-30 16:34:25 +0200

    Update db-app.tf

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -348,3 +348,15 @@ output "db_endpoint" {
   value       = aws_db_instance.default.endpoint
 }
 
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
