e35f68ad3f9f9db4ec59c192e4013a0890ab2da1
Mor Kadosh <kadoshms@gmail.com>
2021-02-01 21:51:31 +0200

    xx

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -12,6 +12,20 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket = "${local.resource_prefix.value}-data-mor5"
+  acl = "private"
+  force_destroy = true
+  tags = {
+    Name = "${local.resource_prefix.value}-data-mor5"
+    Environment = local.resource_prefix.value
+  }
+}
+
 resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
