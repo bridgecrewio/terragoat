be490488cde29a65292515f1f9d4be37e780665b
Mor Kadosh <kadoshms@gmail.com>
2021-02-01 14:20:53 +0200

    test

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -1,18 +1,4 @@
-
 provider "aws" {
-  profile = var.profile
-  region  = var.region
-}
-
-provider "aws" {
-  alias      = "plain_text_access_keys_provider"
-  region     = "us-west-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-}
-
-terraform {
-  backend "s3" {
-    encrypt = true
-  }
+  profile = "dev2"
+  region  = "us-west-2"
 }
