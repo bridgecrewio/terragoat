914c8600f9e58eafa15367fbfb9cf675c57e1350
Mor Kadosh <kadoshms@gmail.com>
2021-02-01 22:01:18 +0200

    arn

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -20,6 +20,7 @@ resource "aws_s3_bucket" "data2" {
   bucket = "${local.resource_prefix.value}-data-mor5"
   acl = "private"
   force_destroy = true
+  arn = ""
   tags = {
     Name = "${local.resource_prefix.value}-data-mor5"
     Environment = local.resource_prefix.value
@@ -30,6 +31,7 @@ resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  arn           = "arn:aws:s3:::619572639823-acme-dev-financials-test-mor4"
   bucket        = "${local.resource_prefix.value}-financials-test-mor4"
   acl           = "private"
   force_destroy = true
