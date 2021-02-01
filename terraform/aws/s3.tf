90e117749d1995f071de84be4286100ac59eba99
Mor Kadosh <kadoshms@gmail.com>
2021-02-01 15:33:21 +0200

    tes

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -3,11 +3,11 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket = "${local.resource_prefix.value}-data"
+  bucket = "${local.resource_prefix.value}-data-mor4"
   acl = "public-read"
   force_destroy = true
   tags = {
-    Name = "${local.resource_prefix.value}-data"
+    Name = "${local.resource_prefix.value}-data-mor4"
     Environment = local.resource_prefix.value
   }
 }
