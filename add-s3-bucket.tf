489ed5b06ffb3b6d55bd74c978a5213b5673f6ee
Raj Paul <rpaul80@gmail.com>
2021-02-16 14:53:19 -0500

    Add a s3 bucket

---
+++ add-s3-bucket.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "new_iam_role" {
+  name = "my-new-iam-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": ["s3:ListBucket"], 
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# this bucket is mean to be public!
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
+
+
+resource aws_ecr_repository "repository" {
+  name                 = "${local.resource_prefix.value}-repository"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name = "${local.resource_prefix.value}-repository"
+  }
+}
