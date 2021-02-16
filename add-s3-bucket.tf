83e9367e34a6c1a417a942513b6b46a4b742bb06
Raj Paul <rpaul80@gmail.com>
2021-02-16 14:56:17 -0500

    encrypt the s3 bucket.

--- add-s3-bucket.tf
+++ add-s3-bucket.tf
@@ -22,6 +22,14 @@ resource "aws_s3_bucket" "data" {
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "private"
   force_destroy = true
+  # provides encryption at-rest without any maintenance required
+  server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+              sse_algorithm = "AES256"
+          }
+      }
+  }
   tags = {
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
