aef641809b6e458d10d328f4e7ecc91d681cc023
Taylor <28880387+tsmithv11@users.noreply.github.com>
2021-03-11 13:18:50 -0800

    Update s3.tf

--- simple_bucket/s3.tf
+++ simple_bucket/s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "storage" {
-  bucket = "storage_bucket"
+  bucket = var.bucket_name
   acl = "public-read-write"
+}
+
+variable "bucket_name" {
+  description = "Name for your S3 bucket (globally unique)"
+  type        = string
+  default     = "my_bucket"
 }
