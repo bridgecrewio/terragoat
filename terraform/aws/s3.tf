6d48412de5d9163924fbeb86fd63c17a047b92d2
Matt Johnson <matt@bridgecrew.io>
2021-04-15 14:56:14 +0100

    add new test s3 bucket based on data bucket

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -15,6 +15,22 @@ resource "aws_s3_bucket" "data" {
   }
 }
 
+resource "aws_s3_bucket" "data2" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+  versioning {
+    enabled = "${var.versioning_enabled}"
+  }
+}
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
