4cd660233f90db1e707554b12a864082afc38b68
Mor Kadosh <kadoshms@gmail.com>
2021-04-18 14:24:07 +0300

    test

--- terraform/misc/s3.tf
+++ terraform/misc/s3.tf
@@ -7,4 +7,16 @@ resource "aws_s3_bucket" "some_bucket" {
     Name = "tag-data-mor5"
     Environment = "env"
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+logging {
+  target_bucket = aws_s3_bucket.some_bucket.id
+  target_prefix = "log/bucket"
 }
