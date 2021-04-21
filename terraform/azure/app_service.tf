b5974816b15c6dfdd8e995a13f89a80d8a9ac0fb
Nick Rodriquez <nicholasrodriquez@gmail.com>
2021-04-20 21:17:52 -0700

    Commited

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -32,3 +32,14 @@ resource azurerm_app_service "app-service2" {
   }
 }
 
+resource azurerm_app_service "app-service2" {
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = var.location
+  name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
+  resource_group_name = azurerm_resource_group.example.name
+  https_only          = true
+
+  auth_settings {
+    enabled = false
+  }
+}
