f97aa2b307ff79cc40a84448d3362458810f3558
Raj Paul <raj@boostsecurity.io>
2021-05-26 09:04:24 -0400

    new

---
+++ new.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
