1745981141d69f33e5da5577f62fe1f8bb133b23
Raj Paul <raj@boostsecurity.io>
2021-05-26 09:40:38 -0400

    should be an issue

--- new.tf
+++ new.tf
@@ -11,3 +11,18 @@ resource "aws_s3_bucket" "data2" {
     Environment = local.resource_prefix.value
   }
 }
+
+
+resource "aws_s3_bucket" "data3" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
