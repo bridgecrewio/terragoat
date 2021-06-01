e91a89259513655f1db3754a5611ad682b27b6fa
stu100wyatt <52220278+stu100wyatt@users.noreply.github.com>
2021-06-01 09:27:32 +0100

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -67,6 +67,14 @@ resource "aws_security_group" "web-node" {
     cidr_blocks = [
     "0.0.0.0/0"]
   }
+  ingress {
+    from_port = 3389  
+    to_port   = 3389
+    protocol  = "tcp"
+    cidr_blocks = [
+    "0.0.0.0/0"]
+  }
+  
   egress {
     from_port = 0
     to_port   = 0
