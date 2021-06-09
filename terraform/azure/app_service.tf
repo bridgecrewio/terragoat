ddc3719be8d689fa90723ac59b0d295b4f2b2b3c
Mor Kadosh <kadoshms@gmail.com>
2021-06-09 11:54:23 +0300

    added suppression

--- terraform/azure/app_service.tf
+++ terraform/azure/app_service.tf
@@ -22,6 +22,18 @@ resource azurerm_app_service "app-service1" {
 }
 
 resource azurerm_app_service "app-service2" {
+  # checkov:skip=BC_AZR_NETWORKING_8:mor test5
+  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = var.location
+  name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
+  resource_group_name = azurerm_resource_group.example.name
+  https_only          = false
+  site_config {
+    min_tls_version = "1.1"
+  }
+}
+
+resource azurerm_app_service "app-service2" {
   app_service_plan_id = azurerm_app_service_plan.example.id
   location            = var.location
   name                = "terragoat-app-service-${var.environment}${random_integer.rnd_int.result}"
