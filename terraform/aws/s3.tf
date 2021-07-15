6b8e3954050efab27a248b0a0aab471b0a17afae
tronxd <tron47@gmail.com>
2021-07-15 11:21:28 +0300

    yoni

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -45,7 +45,7 @@ resource "aws_s3_bucket" "financials" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "private"
+  acl           = "public-read"
   force_destroy = true
   tags = merge({
     Name        = "${local.resource_prefix.value}-financials"
@@ -121,8 +121,7 @@ resource "aws_s3_bucket" "logs" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+        sse_algorithm     = "imayoni"
       }
     }
   }
