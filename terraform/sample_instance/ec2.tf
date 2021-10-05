8c97edc8253fa1aa4c56bd51e157b8b145b6288c
asiosio <91682683+asiosio@users.noreply.github.com>
2021-10-05 15:59:44 +0200

    Update ec2.tf

--- terraform/sample_instance/ec2.tf
+++ terraform/sample_instance/ec2.tf
@@ -27,6 +27,7 @@ resource "aws_security_group" "ssh_traffic" {
 
 resource "aws_instance" "web_server_instance" {
   ami             = data.aws_ami.ubuntu.id
+  vpc_id     = "vpc-0e3c5e2a6a4d7d6a5"
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ssh_traffic.name}"]
   tags = {
