cb366ead661c8dc65aa5e97b687829a6a7dab7ea
lcastrose <44179932+lcastrose@users.noreply.github.com>
2021-10-21 21:21:48 -0500

    Create ec2.tf

---
+++ terraform/simple_instance/ec2.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "ssh_traffic" {
+  name        = "ssh_traffic"
+  description = "Allow SSH inbound traffic"
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_server_instance" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  security_groups = [ "${aws_security_group.ssh_traffic.name}" ]
+  tags = {
+    Name = "bc_workshop_ec2"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
