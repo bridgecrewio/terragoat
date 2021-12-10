f60a4f7c2f74396399b975fdb7cac179c6027f42
Adela Vacaru <80464597+adelavv@users.noreply.github.com>
2021-12-10 14:46:55 +0100

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -33,6 +33,7 @@ resource "aws_ebs_volume" "web_host_storage" {
   }
 }
 
+
 resource "aws_ebs_snapshot" "example_snapshot" {
   # ebs snapshot without encryption
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
