23c55173277fa82302d1ff2672d324487cb42008
LironElbaz <34870196+LironElbaz@users.noreply.github.com>
2021-12-13 08:34:25 +0200

    change bucket name

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
+  bucket        = "${local.resource_prefix.value}-data-changed-name"
   acl           = "public-read"
   force_destroy = true
   tags = merge({
