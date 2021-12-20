a991c119b5de8b5d3058b4e1b0dcf6ea12e01814
Jose Brooks <jbrooks@paloaltontworks.com>
2021-12-20 17:02:56 -0600

    Added a few spaces in the code

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -60,7 +60,7 @@ resource aws_vpc "eks_vpc" {
 }
 
 resource aws_subnet "eks_subnet1" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.eks_vpc.id     
   cidr_block              = "10.10.10.0/24"
   availability_zone       = "${var.region}a"
   map_public_ip_on_launch = true
