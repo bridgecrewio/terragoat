e71607a941b26a02791ab5d5fd53193e37cb45c3
PANW TRAINING <81978904+panwtraining@users.noreply.github.com>
2021-12-21 05:14:01 -0600

    Update ec2.tf
    added spaces behind line of code

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -12,10 +12,10 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
-export AWS_SECRET_ACCESS_KEY=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     
+export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA     
+export AWS_SECRET_ACCESS_KEY=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          
 export AWS_DEFAULT_REGION=us-west-2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html     
 EOF
   tags = merge({
     Name = "${local.resource_prefix.value}-ec2"
