509e7b52c5d72d8a2b67de0b041c8eea0c70e390
kdustin117 <43281203+kdustin117@users.noreply.github.com>
2022-01-06 15:32:44 -0500

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -31,14 +31,12 @@ EOF
   })
   monitoring = true
   ebs_optimized = true
-  *_block_device {
-    encrypted = true
-  }
 }
 
 
 
 resource "aws_ebs_volume" "web_host_storage" {
+	# checkov:skip=CKV_AWS_3: Will encrypt in UAT
   # unencrypted volume
   availability_zone = "${var.region}a"
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
@@ -143,7 +141,6 @@ resource "aws_subnet" "web_subnet" {
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.10.0/24"
   availability_zone       = "${var.region}a"
-  map_public_ip_on_launch = true
 
   tags = merge({
     Name = "${local.resource_prefix.value}-subnet"
@@ -158,6 +155,7 @@ resource "aws_subnet" "web_subnet" {
     yor_trace            = "0345f650-d280-4ca8-86c9-c71c38c0eda8"
   })
 }
+
 
 resource "aws_subnet" "web_subnet2" {
   vpc_id                  = aws_vpc.web_vpc.id
