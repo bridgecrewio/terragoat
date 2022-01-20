a1d1c1ce31a1bde6dafa188846d90eca82abe5fd
Taylor <28880387+tsmithv11@users.noreply.github.com>
2022-01-19 21:32:41 -0800

    Create mssql.tf
    Adds a file with passing resources for CKV_AZURE_26 and 1 failing resource so Smart Fixes are triggered

---
+++ terraform/azure/mssql.tf
@@ -0,0 +1,182 @@
+resource "azurerm_storage_account" "security_storage_account" {
+  name                     = "securitystorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+}
+
+resource "azurerm_mssql_server" "mssql1" {
+  name                         = "mssql1"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql2" {
+  name                         = "mssql2"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql3" {
+  name                         = "mssql3"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql4" {
+  name                         = "mssql4"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql5" {
+  name                         = "mssql5"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql6" {
+  name                         = "mssql6"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server" "mssql7" {
+  name                         = "mssql7"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "missadministrator"
+  administrator_login_password = "AdminPassword123!"
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy1" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql1.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy2" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql2.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy3" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql3.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy4" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql4.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy5" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql5.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy6" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql6.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
+
+resource "azurerm_mssql_server_security_alert_policy" "alertpolicy7" {
+	# checkov:skip=CKV_AZURE_27: Other contact
+  # checkov:skip=CKV_AZURE_25: Other contact
+  resource_group_name        = azurerm_resource_group.example.name
+  server_name                = azurerm_mssql_server.mssql7.name
+  state                      = "Enabled"
+  storage_endpoint           = azurerm_storage_account.security_storage_account.primary_blob_endpoint
+  storage_account_access_key = azurerm_storage_account.security_storage_account.primary_access_key
+  disabled_alerts = [
+    "Sql_Injection",
+    "Data_Exfiltration"
+  ]
+  retention_days = 20
+  email_addresses = ["securityengineer@bridgecrew.io"]
+}
