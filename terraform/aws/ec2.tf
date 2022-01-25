0af6fc74c64fca287a7e4ac3013aa61759f2ca26
Ivan Tresoldi <johndoe@example.com>
2022-01-25 12:13:03 +0100

    EBS Encryption set to true

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -48,7 +48,9 @@ resource "aws_ebs_volume" "web_host_storage" {
     git_repo             = "ivan-terragoat"
     yor_trace            = "c5509daf-10f0-46af-9e03-41989212521d"
   })
+  encrypted = true
 }
+
 
 resource "aws_ebs_snapshot" "example_snapshot" {
   # ebs snapshot without encryption
