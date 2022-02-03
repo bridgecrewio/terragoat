2084ff3388246a415c91c9576efd45da22bbac1e
steven-deboer <96180461+steven-deboer@users.noreply.github.com>
2022-02-03 09:55:15 +0100

    Update terraform/sb.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform/sb.tf
+++ terraform/sb.tf
@@ -1,5 +1,12 @@
 
 resource "aws_s3_bucket" "steven" {
+   server_side_encryption_configuration {
+     rule {
+       apply_server_side_encryption_by_default {
+         sse_algorithm = "aws:kms"
+       }
+     }
+   }
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
