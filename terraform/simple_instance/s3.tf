7af857a31b28aba9f7b6b451aec76d3e92e532c7
OptimAdam <OptimAdam@hotmail.com>
2022-02-23 10:28:12 -0800

    Update terraform/simple_instance/s3.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "docking_bay" {
+  versioning {
+    enabled = true
+  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
