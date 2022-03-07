6942572618b934c19079d3e8256f2543d1ccbeef
Peleg Litvak <peleg.litvak@gmail.com>
2022-03-07 16:54:39 +0200

    Update ecr.tf

--- terraform/aws/ecr.tf
+++ terraform/aws/ecr.tf
@@ -6,7 +6,7 @@ resource aws_ecr_repository "repository" {
     Name = "${local.resource_prefix.value}-repository"
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/ecr.tf"
+    git_file             = "terraform/aws/foo/ecr.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
