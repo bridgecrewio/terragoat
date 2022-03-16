851ef66fcb4403ccd8108906f28f3f43362034eb
matansha <matanshati@gmail.com>
2022-03-16 20:09:36 +0200

    Update db-app.tf

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -27,7 +27,7 @@ resource "aws_db_instance" "default" {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/db-app.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
+    git_last_modified_by = "nimrodkor2@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
