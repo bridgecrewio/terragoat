a8116e301bdac11d9b34538d24f69b07f231dda2
hadighz <hadi.ghz@gmail.com>
2022-03-29 14:25:59 +1100

    resource ec2 added to test policy

---
+++ terraform-aws/ec2-hacked.tf
@@ -0,0 +1,7 @@
+resource "aws_ec2_host" "test" {
+  instance_type     = "c5.18xlarge"
+  availability_zone = "us-west-2a"
+  host_recovery     = "on"
+  auto_placement    = "on"
+  tags = "hacked"
+}
