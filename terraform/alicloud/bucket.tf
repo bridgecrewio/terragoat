9c114f23d311f787c137723e1f71b27a52f0adec
James Woolfenden <james.woolfenden@gmail.com>
2022-04-05 16:17:55 +0100

    Aligoat

--- terraform/alicloud/bucket.tf
+++ terraform/alicloud/bucket.tf
@@ -1,6 +1,6 @@
 resource "alicloud_oss_bucket" "bad_bucket" {
   # Public and writeable bucket 
-  # Versioning enabled
+  # Versioning isn't enabled
   # Not Encrypted with a Customer Master Key and no Server side encryption
   # Doesn't have access logging enabled" 
   bucket = "wildwestfreeforall"
