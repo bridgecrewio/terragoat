5406d83ae1a7c43b5f1f44a0d044f4622f4a815d
James Woolfenden <james.woolfenden@gmail.com>
2022-04-06 16:27:36 +0100

    split out

--- terraform/oracle/bucket.tf
+++ terraform/oracle/bucket.tf
@@ -12,27 +12,5 @@ resource "oci_objectstorage_bucket" "secretsquirrel" {
   storage_tier          = "Standard"
 }
 
-resource "oci_identity_compartment" "tf-compartment" {
-  compartment_id = var.tenancy_id
-  description    = "Compartment for Terraform resources."
-  name           = "third-compartment"
-  enable_delete  = true
-}
 
-data "oci_objectstorage_namespace" "example" {
-  compartment_id = oci_identity_compartment.tf-compartment.id
-}
 
-variable "tenancy_id" {
-  description = "value supplied by env var"
-  default     = ""
-}
-
-provider "oci" {
-  #these to be provided by env vars
-  # private_key_path
-  # fingerprint
-  # tenancy_ocid 
-  # user_ocid   
-  region = "uk-london-1"
-}
