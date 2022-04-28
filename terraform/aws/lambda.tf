7b3063df7ec08c9dcbbb78df14400e9c023ae1ea
hilaeliezer <hila2575@gmail.com>
2022-04-28 14:43:50 +0300

    Update lambda.tf
    hila test

--- terraform/aws/lambda.tf
+++ terraform/aws/lambda.tf
@@ -28,6 +28,34 @@ EOF
   }
 }
 
+resource "aws_lambda_function" "analysis_lambda123" {
+  # lambda have plain text secrets in environment variables
+  filename      = "resources/lambda_function_payload.zip"
+  function_name = "${local.resource_prefix.value}-analysis"
+  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  handler       = "exports.test"
+
+  source_code_hash = "${filebase64sha256("resources/lambda_function_payload.zip")}"
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      access_key = "AKIAIOSFODNN7EXAMPLE"
+      secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    }
+  }
+  tags = {
+    git_commit           = "5c6b5d60a8aa63a5d37e60f15185d13a967f0542"
+    git_file             = "terraform/aws/lambda.tf"
+    git_last_modified_at = "2021-05-02 10:06:10"
+    git_last_modified_by = "nimrodkor@users.noreply.github.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "f7d8bc47-e5d9-4b09-9d8f-e7b9724d826e"
+  }
+}
 resource "aws_lambda_function" "analysis_lambda" {
   # lambda have plain text secrets in environment variables
   filename      = "resources/lambda_function_payload.zip"
