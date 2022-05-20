cfab6723396b6d0c8fb455dce81bd6fc3510b5a0
stu100wyatt <52220278+stu100wyatt@users.noreply.github.com>
2022-05-20 09:49:28 +0100

    Update terraform/aws/newec2.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/newec2.tf
+++ terraform/aws/newec2.tf
@@ -137,7 +137,6 @@ resource "aws_subnet" "web_subnet" {
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.10.0/24"
   availability_zone       = "${var.region}a"
-  map_public_ip_on_launch = true
 
   tags = merge({
     Name = "${local.resource_prefix.value}-subnet"
