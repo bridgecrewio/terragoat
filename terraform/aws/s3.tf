b1acc1ac9ee9c316616f2364c7a70c4804da4590
lcastrose <44179932+lcastrose@users.noreply.github.com>
2022-06-07 11:14:53 -0500

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  force_destroy = true
+  force_destroy = false
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
