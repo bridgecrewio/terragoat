c2a9986dbf9430882b9620c04d2414b1d56d9c9f
reuvenz1993 <rovenroven1@gmail.com>
2022-06-08 17:43:21 +0300

    new change 8_6_2022

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -46,20 +46,6 @@ resource "aws_s3_bucket" "financials" {
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-financials"
   acl           = "private"
-  force_destroy = true
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-financials"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "0e012640-b597-4e5d-9378-d4b584aea913"
-  })
 
 }
 
@@ -100,6 +86,7 @@ resource "aws_s3_bucket" "data_science" {
     target_prefix = "log/"
   }
   force_destroy = true
+
   tags = {
     some_new_key         = "bla bla"
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
