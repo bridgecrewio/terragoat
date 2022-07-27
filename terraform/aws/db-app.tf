4159a2d2365c07a2e0c0066a89a16a424cca7d58
OfekShimk <70918223+OfekShimk@users.noreply.github.com>
2022-07-27 09:47:25 +0300

    Update db-app.tf

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -9,7 +9,6 @@ resource "aws_db_instance" "default" {
   identifier              = "rds-${local.resource_prefix.value}"
   engine_version          = "8.0" # Latest major version 
   instance_class          = "db.t3.micro"
-  allocated_storage       = "20"
   username                = "admin"
   password                = var.password
   apply_immediately       = true
