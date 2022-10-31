d03163ce9b074a504d31e5ed7a22c837b82315d6
Liron Elbaz <34870196+LironElbaz@users.noreply.github.com>
2022-10-31 08:51:41 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "data" {
+resource "aws_s3_bucket" "data-2" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
+  bucket        = "${local.resource_prefix.value}-data-2"
   acl           = "public-read"
   force_destroy = true
   tags = merge({
-    Name        = "${local.resource_prefix.value}-data"
+    Name        = "${local.resource_prefix.value}-data-2"
     Environment = local.resource_prefix.value
     }, {
     git_commit           = "4d57f83ca4d3a78a44fb36d1dcf0d23983fa44f5"
