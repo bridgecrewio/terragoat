3af2afe24843cf284799bef9e0206d93b35e7232
Bugidi Venkata Sai Kiran <94594083+Kiransai321@users.noreply.github.com>
2023-01-19 16:40:25 +0530

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
-  instance_type = "t3.nano"
+  instance_type = "t3.micro"
 
   vpc_security_group_ids = [
   "${aws_security_group.web-node.id}"]
@@ -14,7 +14,7 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
 export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-west-2
+export AWS_DEFAULT_REGION=us-west-1
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
   tags = merge({
