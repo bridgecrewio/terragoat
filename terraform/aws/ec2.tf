26db18f118c20fb7bde5d3403a97002f92402fee
karanb141 <63932525+karanb141@users.noreply.github.com>
2023-01-31 16:44:16 -0600

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "web_host" {
   # ec2 have plain text secrets in user data
-  ami           = "${var.ami}"
+  ami           = "${var.ami}". 
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
   "${aws_security_group.web-node.id}"]
-  subnet_id = "${aws_subnet.web_subnet.id}"
+  subnet_id = "${aws_subnet.web_subnet.id}".  
   user_data = <<EOF
 #! /bin/bash
 sudo apt-get update
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2.    
 sudo systemctl start apache2
 sudo systemctl enable apache2
 export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
