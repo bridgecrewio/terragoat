3bc0359bb9d7d9e810a6028321c8deca8081d68f
zohebsarwar82 <58395751+zohebsarwar82@users.noreply.github.com>
2023-02-03 20:03:21 +1100

    Update kms.tf

--- terraform/aws/kms.tf
+++ terraform/aws/kms.tf
@@ -10,7 +10,7 @@ resource "aws_kms_key" "logs_key" {
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
-    git_repo             = "terrag1oat"
+    git_repo             = "terrag22at"
     yor_trace            = "cd8fa2a7-4868-4cd1-993d-da4644808ce5"
     name                 = "zs"
   }
