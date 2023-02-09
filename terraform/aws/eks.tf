59dea1b18b503c197a5cebc02d4c4150ad587689
Bugidi Venkata Sai Kiran <94594083+Kiransai321@users.noreply.github.com>
2023-02-09 15:44:27 +0530

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -49,7 +49,7 @@ resource aws_vpc "eks_vpc" {
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/eks.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_at = "2020-06-16 14:46:54"
     git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
