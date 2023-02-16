bb104084c3fcd0787e6daeebbcee1c98e67d0221
Pratik.Dutta <pratik.dutta@bakerhuges.com>
2023-02-16 17:17:40 +0530

    This is test change- Student36

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "data" {
+	# checkov:skip=CKV2_AWS_6: ADD REASON
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
@@ -19,6 +20,21 @@ resource "aws_s3_bucket" "data" {
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
 }
+
+
+resource "aws_s3_bucket" "data_log_bucket" {
+  bucket = "data-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "data" {
+  bucket = aws_s3_bucket.data.id
+
+  target_bucket = aws_s3_bucket.data_log_bucket.id
+  target_prefix = "log/"
+}
+
+
+
 
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
