a119a232a4f9cd2b3ffcca308473f6572766fca0
manjuvp <manjunath.vp@outlook.com>
2023-02-17 04:12:03 -0800

    my test

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "data" {
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
 }
-
+//test
 resource "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
