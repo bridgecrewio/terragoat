bad5293030fd5745813d5252e375f52a8091e6f3
Dustin Krysak <dustin@bashfulrobot.com>
2023-03-28 10:26:56 -0700

    refactor: :recycle: add comment to TF file Changes to be committed: 	modified:   terraform/alicloud/bucket.tf

--- terraform/alicloud/bucket.tf
+++ terraform/alicloud/bucket.tf
@@ -1,8 +1,9 @@
+# test trigger
 resource "alicloud_oss_bucket" "bad_bucket" {
-  # Public and writeable bucket 
+  # Public and writeable bucket
   # Versioning isn't enabled
   # Not Encrypted with a Customer Master Key and no Server side encryption
-  # Doesn't have access logging enabled" 
+  # Doesn't have access logging enabled"
   bucket = "wildwestfreeforall"
   acl    = "public-read-write"
   tags = {
