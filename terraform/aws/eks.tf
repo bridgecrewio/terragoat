2f65d8402e578d305e23d2307df0d8f495209755
abdus quadri <asquadri1@gmail.com>
2023-04-17 13:44:28 -0500

    test

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -43,7 +43,7 @@ resource aws_iam_role_policy_attachment "policy_attachment-AmazonEKSServicePolic
 resource aws_vpc "eks_vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support   = false
   tags = merge({
     Name = "${local.resource_prefix.value}-eks-vpc"
     }, {
