7b1d296d2f8d094a573078612ce921d7427a0d05
Pavel Kerbel <kerbelp@gmail.com>
2023-04-17 11:17:20 +0300

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -23,9 +23,9 @@ EOF
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/ec2.tf"
     git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
+    git_last_modified_by = "kerbelp@gmail.com"
+    git_modifiers        = "kerbelp"
+    git_org              = "kerbelp"
     git_repo             = "terragoat"
     yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
   })
