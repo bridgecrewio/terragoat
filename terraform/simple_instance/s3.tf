928adc70a6ea31e6fa4be5bfb0bf5baa0305bf44
Ariel Salvo <arielsalvo@users.noreply.github.com>
2023-05-18 22:37:24 -0300

    Create s3.tf

---
+++ terraform/simple_instance/s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "dockingbay" {
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
+  }
+}
