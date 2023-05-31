fbd0c9b48aca29fbf0bde8556f0c40ef96d5cadf
vmbarga26750 <vmbarga26750@gmail.com>
2023-05-31 17:05:49 +0200

    Trigger this

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_object" "data_object" {
   })
 }
 
-resource "aws_s3_bucket" "financials" {
+resource "aws_s3_bucket" "financials-v2" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
