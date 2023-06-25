49de9a5ac1b1268ee4bde427e4fc554536b4b841
chiangyaw8 <chiangyaw@live.com>
2023-06-25 13:29:34 +0000

    added new s3 bucket

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -6,7 +6,8 @@ resource "aws_s3_bucket" "prismaclouds3" {
   bucket_prefix = "prismacloud-s3"
 
   tags = {
-    Name                 = "Prisma Cloud"
-    Environment          = "Dev"
+    Name        = "Prisma Cloud"
+    Environment = "Dev"
+    yor_trace   = "60eeb16c-a434-48b7-b367-8c5c7dbd1821"
   }
 }
