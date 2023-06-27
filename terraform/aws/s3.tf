d19b121569e587023a335391baa437c0f1d33db6
Joshua Levin <joshua.levin@momentum.co.za>
2023-06-27 22:54:54 +0200

    applied versioning fix

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -18,6 +18,10 @@ resource "aws_s3_bucket" "data" {
     git_repo             = "terragoat"
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket_object" "data_object" {
