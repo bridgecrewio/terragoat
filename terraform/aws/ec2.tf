3c581205d986330bd64e86dceb41a1d2a2204f40
awrightt <117774326+awrightt@users.noreply.github.com>
2023-06-28 12:57:53 -0500

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAA
+export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMAAB
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
