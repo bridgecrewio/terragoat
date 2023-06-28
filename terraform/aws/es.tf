71f555dec5d05f4eab88437ebe83c7c0a9b10ba8
awrightt <117774326+awrightt@users.noreply.github.com>
2023-06-28 12:53:22 -0500

    Update es.tf

--- terraform/aws/es.tf
+++ terraform/aws/es.tf
@@ -1,14 +1,14 @@
-resource "aws_elasticsearch_domain" "monitoring-framework" {
-  domain_name           = "tg-${var.environment}-es"
-  elasticsearch_version = "2.3"
+# resource "aws_elasticsearch_domain" "monitoring-framework" {
+#   domain_name           = "tg-${var.environment}-es"
+#   elasticsearch_version = "2.3"
 
-  cluster_config {
-    instance_type            = "t2.small.elasticsearch"
-    instance_count           = 1
-    dedicated_master_enabled = false
-    dedicated_master_type    = "m4.large.elasticsearch"
-    dedicated_master_count   = 1
-  }
+#   cluster_config {
+#     instance_type            = "t2.small.elasticsearch"
+#     instance_count           = 1
+#     dedicated_master_enabled = false
+#     dedicated_master_type    = "m4.large.elasticsearch"
+#     dedicated_master_count   = 1
+#   }
 
   ebs_options {
     ebs_enabled = true
