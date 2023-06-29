6ff657bc9ecfda711a6d11ac9974931bccfae55d
jmagee70 <jmagee@paloaltonetworks.com>
2023-06-29 16:25:03 +0100

    new secrets

---
+++ Secrets/app1.js
@@ -0,0 +1,3 @@
+const SEC_1 = "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"
+const SEC_2 = "eyJrIjoiNUwyZU7TMmRxQXNVcnR7UXB0ME4zYkhRaTk2STVhR0MiLCJuIjoidGVtcCIsImlkIjoxfQ=="
+const SEC_3 = "dsapi45202d12abdce73c004a9e0be24a21b2"
