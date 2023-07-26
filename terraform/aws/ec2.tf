aa0362ae3f42413d3f1ed1c0b329680e8fbd6c13
hlevian <135955824+hlevian@users.noreply.github.com>
2023-07-26 15:59:37 +0300

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -18,7 +18,7 @@ export AWS_DEFAULT_REGION=us-west-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
   tags = merge({
-    Name = "${local.resource_prefix.value}-ec2"
+    Name = "${local.resource_prefix.value}-ec2-2"
     }, {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file             = "terraform/aws/ec2.tf"
