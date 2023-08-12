9eed3ffefc52b8f42069aaff3488dd7d737341db
Patrick Burke <24pburke@gmail.com>
2023-08-12 11:30:39 -0500

    Update variables.tf with azure client secret value, azure client secret id

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -13,3 +13,13 @@ variable "environment" {
   default     = "dev"
   description = "Must be all lowercase letters or numbers"
 }
+
+variable "test_client_secret_value" {
+  default     = "Q598Q~cGWt_XhN1x9oMEn7z3TsG9Ph4H5qBU~aio"
+  type = string
+}
+
+variable "test_client_secret_id" {
+  default     = "80bdb59f-288f-4cb3-ae6a-3d9a979e6a6e"
+  type = string
+}
