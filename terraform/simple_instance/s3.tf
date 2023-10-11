1492d2ca60ff6810ea8d7d56b9e52d4958c8a6ff
Mohammed ABDELOUHAB <mabdelouhab@gmail.com>
2023-10-11 23:17:34 +0200

    Create s3.tf

---
+++ terraform/simple_instance/s3.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_s3_bucket" "dockingbay" {
+  bucket_prefix = "docking-bay-storage-"
+
+  tags = {
+    Name                 = "Docking Bay"
+    Environment          = "Dev"
+  }
+}
