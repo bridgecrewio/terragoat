edcacfb8196c794465c57cede62d133558b7b4b0
gndupalo <113545731+gndupalo@users.noreply.github.com>
2024-03-11 15:41:53 -0400

    Update ec2.tf
    Creating test on EC2

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web_host" {
-  # ec2 have plain text secrets in user data
+  # ec2 have plain text secrets in user data with george being specific
   ami           = "${var.ami}"
   instance_type = "t2.nano"
 
