472f25b40dc56c9262eb1deaa015db99a69489e4
Pavlo Chebotiuk <chebotuk@gmail.com>
2026-06-14 18:44:18 +0200

    test: add wide-open SSH security group

---
+++ insecure-test.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group" "wide_open" {
+  name = "wide_open_sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
